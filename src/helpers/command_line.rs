9f2963fed3f494fe5176c027cb775c58e034d51d
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-07-16 16:16:25 +0530

    Add std out

--- src/helpers/command_line.rs
+++ src/helpers/command_line.rs
@@ -4,6 +4,42 @@ use crossterm::{
 };
 use std::io::{stdin, stdout};
 
+#[derive(PartialEq, Debug)]
+pub enum PrintCommand {
+    AICall,
+    UnitTest,
+    Issue,
+}
+
+impl PrintCommand {
+    pub fn print_agent_message(self, agent_pos: &str, agent_statement: &str) {
+        let mut stdout: std::io::Stdout = stdout();
+
+        let statement_color: Color = match self {
+            Self::AICall => Color::Cyan,
+            PrintCommand::UnitTest => Color::Magenta,
+            PrintCommand::Issue => Color::Red,
+        };
+
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        print!("Agent: {}: ", agent_pos);
+
+        stdout.execute(SetForegroundColor(statement_color)).unwrap();
+        print!("{} \n", agent_statement);
+
+        stdout.execute(ResetColor).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_print_agent_msg() {
+        PrintCommand::AICall.print_agent_message("Managing Agent", "Processing request here");
+    }
+}
 //  Get user request
 pub fn get_user_response(question: &str) -> String {
     let mut stdout: std::io::Stdout = stdout();
