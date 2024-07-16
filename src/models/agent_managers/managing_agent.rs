c7a792e0ef082256dbb78e8faa548cf8cff1b00a
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-07-16 19:44:02 +0530

    init manager

--- src/models/agent_managers/managing_agent.rs
+++ src/models/agent_managers/managing_agent.rs
@@ -1,6 +1,10 @@
-use crate::models::{
-    agent_basic::basic_agent::BasicAgent,
-    agents::agent_traits::{FactSheet, SpecialFunctions},
+use crate::{
+    ai_functions::aifunc_managing::convert_user_input_to_goal,
+    helpers::general::ai_task_request,
+    models::{
+        agent_basic::basic_agent::{AgentState, BasicAgent},
+        agents::agent_traits::{FactSheet, SpecialFunctions},
+    },
 };
 
 #[derive(Debug)]
@@ -8,4 +12,40 @@ pub struct ManagingAgent {
     attributes: BasicAgent,
     factsheet: FactSheet,
     agents: Vec<Box<dyn SpecialFunctions>>,
+}
+
+impl ManagingAgent {
+    pub async fn new(usr_req: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let position: String = "Project Manager".to_string();
+        let attributes: BasicAgent = BasicAgent {
+            memory: vec![],
+            objective: "Manage agents who are building an execellent website for the user"
+                .to_string(),
+            position: position.clone(),
+            state: AgentState::Discovery,
+        };
+
+        let project_description: String = ai_task_request(
+            usr_req,
+            &position,
+            get_function_string!(convert_user_input_to_goal),
+            convert_user_input_to_goal,
+        )
+        .await;
+
+        let agents: Vec<Box<dyn SpecialFunctions>> = vec![];
+        let factsheet: FactSheet = FactSheet {
+            project_description,
+            project_scope: None,
+            external_url: None,
+            backend_code: None,
+            api_endpoint_schema: None,
+        };
+
+        Ok(Self {
+            agents,
+            attributes,
+            factsheet,
+        })
+    }
 }
