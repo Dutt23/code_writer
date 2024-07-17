15820cf4c257c064b9d6cffd64a9c93355b9da5d
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2024-07-17 17:44:37 +0530

    Start backend dev

---
+++ src/models/agents/agent_backend.rs
@@ -0,0 +1,66 @@
+use crate::{
+    ai_functions::aifunc_backend::{print_backend_webserver_code, print_improved_webserver_code},
+    helpers::general::{ai_task_request, read_code_template_contents, save_backend_code},
+    models::agent_basic::{basic_agent::BasicAgent, basic_traits::BasicTrait},
+};
+
+use super::agent_traits::FactSheet;
+
+#[derive(Debug)]
+pub struct AgentBackendDeveloper {
+    attributes: BasicAgent,
+    bug_errors: Option<String>,
+    bug_count: u8,
+}
+
+impl AgentBackendDeveloper {
+    pub fn new() -> Self {
+        let attributes = BasicAgent::new(
+            "Develops backend code for websever and json database".to_string(),
+            "Backend developer".to_string(),
+        );
+        Self {
+            attributes,
+            bug_count: 0,
+            bug_errors: None,
+        }
+    }
+
+    async fn call_initial_backend_code(&mut self, factsheet: &mut FactSheet) {
+        let code_template_str: String = read_code_template_contents();
+
+        let msg_context: String = format!(
+            "CODE TEMPLATE {} \n PROJECT_DESCRIPTION {} \n",
+            code_template_str, factsheet.project_description
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_backend_webserver_code),
+            print_backend_webserver_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+
+    async fn call_improved_backend_code(&mut self, factsheet: &mut FactSheet) {
+        let msg_context: String = format!(
+            "CODE TEMPLATE {:?} \n PROJECT_DESCRIPTION {:?} \n",
+            factsheet.backend_code, factsheet
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_improved_webserver_code),
+            print_improved_webserver_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+}
